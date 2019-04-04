50ff5e031ff1feedb1601947af1dea5638dfda19
longchau <longchau21@gmail.com>
2019-04-04 09:37:00 +0300

    Add more tests skeleton

---
+++ tests/test_game.py
@@ -0,0 +1,12 @@
+'''
+Test Game object in cotuong.py
+''' 
+
+from unittest import TestCase 
+
+class TestGame(TestCase):
+    def setUp(self):
+        pass 
+
+    def test_create_fresh_game(self):
+        pass 
