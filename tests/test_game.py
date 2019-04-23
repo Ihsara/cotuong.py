db733787708fa3d1acb12ead8515f3d7225f9327
Ihsara <longchau21@gmail.com>
2019-04-23 22:24:29 +0300

    Remove GameState().board_setup

--- tests/test_game.py
+++ tests/test_game.py
@@ -13,14 +13,12 @@ class TestGame(TestCase):
 
     def test_create_fresh_game(self):
         new_game = GameState()
-        self.assertEqual(new_game.board_setup, start_coords_2)
         self.assertEqual(new_game.board_matrix, board_matrix)
         self.assertEqual(new_game.history, [])
         self.assertEqual(new_game.next_move, 'w')
         self.assertEqual(new_game.turn, 1)
 
         new_game.new_game()
-        self.assertEqual(new_game.board_setup, start_coords_2)
         self.assertEqual(new_game.board_matrix, board_matrix)
         self.assertEqual(new_game.history, [])
         self.assertEqual(new_game.next_move, 'w')
